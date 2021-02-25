f1071f6c492c412ea32671ab2857b3dd2e465f42
JohnHernCode <besthostandseoinfo@gmail.com>
2021-02-25 15:35:15 -0600

    Add Form Columns, Form views, bootstrap, params.permit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!
 
   # GET /posts or /posts.json
   def index
@@ -12,7 +13,7 @@ class PostsController < ApplicationController
 
   # GET /posts/new
   def new
-    @post = Post.new
+    @post = Post.new(post_params)
   end
 
   # GET /posts/1/edit
@@ -21,6 +22,7 @@ class PostsController < ApplicationController
 
   # POST /posts or /posts.json
   def create
+    params.permit!
     @post = Post.new(post_params)
 
     respond_to do |format|
