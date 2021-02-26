5b6ace6fc262c1081445c569e8d0fe1dd1d30207
JohnHernCode <besthostandseoinfo@gmail.com>
2021-02-26 10:50:56 -0600

    Add user to the post table, hide fields when not logged in, hide edit fields if not post creator

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -3,14 +3,15 @@ class RegistrationController < ApplicationController
   private
   
   def sign_up_params
-    params.require(:user).permit(:username, :email, :password, :password_confirmation)
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 
   def account_update_params
-    params.require(:user).permit(:username, :email, :password, :password_confirmation, :current_password)
+    params.require(:user).permit(:email, :password, :password_confirmation, :current_password)
   end
 
   def sign_in_params
-    params.require(:user).permit(:username, :password, :remember_me)
+    params.require(:user).permit(:email, :password, :remember_me)
   end
+
 end
